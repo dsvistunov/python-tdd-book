7753ea2f20c70de6e93f781c23dd55f022c1fff4
dsvistunov <d.svistuno0v1991@gmail.com>
2018-02-19 13:36:39 +0200

    Change to ViewAndAddToList, NewListView, MyListView

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView, DetailView
 from lists.forms import ItemForm, ExistingListItemForm, NewListForm
 from lists.models import Item, List
 
@@ -12,29 +12,28 @@ class HomePageView(FormView):
     form_class = ItemForm
 
 
-# def home_page(request):
-#     return render(request, 'home.html', {'form': ItemForm()})
+class ViewAndAddToList(DetailView, CreateView):
+    model = List
+    template_name = 'list.html'
+    form_class = ExistingListItemForm
+
+    def get_form(self):
+        self.object = self.get_object()
+        return self.form_class(for_list=self.object, data=self.request.POST)
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ExistingListItemForm(for_list=list_)
-    if request.method == 'POST':
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(list_)
-    return render(request, 'list.html', {'list': list_, 'form': form})
+class NewListView(CreateView):
+    form_class = NewListForm
+    template_name = 'home.html'
 
-
-def new_list(request):
-    form = NewListForm(data=request.POST)
-    if form.is_valid():
-        list_ = form.save(owner=request.user)
+    def form_valid(self, form):
+        list_ = form.save(owner=self.request.user)
         return redirect(list_)
-    return render(request, 'home.html', {'form': form})
 
 
-def my_lists(request, email):
-    owner = User.objects.get(email=email)
-    return render(request, 'my_lists.html', {'owner': owner})
+class MyListView(DetailView):
+    model = User
+    template_name = 'my_lists.html'
+    context_object_name = 'owner'
+
+
